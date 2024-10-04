44c7902ce95d084790e3b0e5c28d200b1dc74f22
Zaki Ghafari <zaki.ghafari@outlook.de>
2024-10-04 17:12:24 +0200

    initialier Commit

---
+++ src/zoo/Bären.java
@@ -0,0 +1,17 @@
+package zoo;
+
+public class Bären {
+	
+	private String name;
+	private int alter;
+	
+	public Bären(String name, int alter) {
+		this.name = name;
+		this.alter = alter;
+	}
+	
+	public void fighting() {
+		System.out.println("Dropkicks other bear to the stomach");
+	}
+
+}
