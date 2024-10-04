d7c7bdb3a69381fab350d5e883f253dbf0332aa1
Zaki Ghafari <zaki.ghafari@outlook.de>
2024-10-04 17:14:10 +0200

    neue Klasse Tiger wurde hinzugefügt :D

---
+++ src/zoo/Tiger.java
@@ -0,0 +1,14 @@
+package zoo;
+
+public class Tiger {
+
+	private String name;
+	private int alter;
+	
+	public Tiger(String name, int alter) {
+		
+		this.name = name;
+		this.alter = alter;
+		
+	}
+}
