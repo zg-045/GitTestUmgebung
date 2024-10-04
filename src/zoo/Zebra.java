80f375a859204755dc02bd7f071e33ad605a701c
Zaki Ghafari <zaki.ghafari@outlook.de>
2024-10-04 17:15:16 +0200

    neue Klasse Zebra wurde erstellt :D

---
+++ src/zoo/Zebra.java
@@ -0,0 +1,15 @@
+package zoo;
+
+public class Zebra {
+	
+	private String name;
+	private int alter;
+	
+	public Zebra(String name, int alter) {
+		
+		this.name = name;
+		this.alter = alter;
+		
+	}
+
+}
